9a43281536c7ff84e94f749f461f67e46454ad29
Luis Alva <96627296+Luis102487@users.noreply.github.com>
2024-04-29 23:00:52 -0400

    Update SQL_queries.sql

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -1,1 +1,8 @@
+-- DATA CLEANING
+-- Checking for duplicate records
+select project_id, count(project_id) as project_count
+from luisalva.crowdfunding_dataset.projects
+group by project_id
+order by project_count;
+
 
