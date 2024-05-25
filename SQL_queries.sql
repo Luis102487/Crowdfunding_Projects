4159cbee41e33166a95167f5bf1701e3ba7970b8
Luis Alva <96627296+Luis102487@users.noreply.github.com>
2024-05-25 19:05:54 -0400

    Update SQL_queries.sql

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -9,57 +9,6 @@ GROUP BY
 ORDER BY
   project_count DESC;
 
--- What projects category has the largest amount of suceess?
-SELECT
-  category,
-  COUNT(category) AS category_count
-FROM
-  luisalva.crowdfunding_dataset.projects
-WHERE
-  status = 'Successful'
-GROUP BY
-  category
-ORDER BY
-  category_count DESC
-LIMIT
-  1;
-
-
--- Average backers by category
-SELECT
-  category,
-  ROUND(AVG(backers), 2) AS avg_backers
-FROM
-  luisalva.crowdfunding_dataset.projects
-GROUP BY
-  category
-ORDER BY
-  avg_backers DESC;
-
-  
--- Projects by country
-SELECT
-  country,
-  COUNT(project_id) AS project_count
-FROM
-  luisalva.crowdfunding_dataset.projects
-GROUP BY
-  country
-ORDER BY
-  project_count DESC;
-
-
--- Country with most backers 
-SELECT
-  country,
-  SUM(backers) AS avg_backers
-FROM
-  luisalva.crowdfunding_dataset.projects
-GROUP BY
-  country
-ORDER BY
-  avg_backers DESC;
-
 
 -- Projects status percentage
 SELECT
@@ -78,7 +27,28 @@ ORDER BY
   status_percentage DESC;
 
 
--- Category with most money pleadge
+-- What projects category has the largest amount of suceess?
+SELECT
+  category,
+  COUNT(category) AS category_count
+FROM
+  luisalva.crowdfunding_dataset.projects
+WHERE
+  status = 'Successful'
+GROUP BY
+  category
+ORDER BY
+  category_count DESC
+LIMIT
+  1;
+
+
+-- Average goal per category
+
+--- succesful project with large goals/small goal
+
+
+-- Money pledged by category
 SELECT
   category,
   SUM(pledged) AS pledge_total
@@ -88,33 +58,6 @@ GROUP BY
   category
 ORDER BY
   pledge_total;
-
-
--- Countries with most succesful projects
-SELECT
-  country,
-  COUNT(status) AS success_count
-FROM
-  luisalva.crowdfunding_dataset.projects
-WHERE
-  status = 'Successful'
-GROUP BY
-  country
-ORDER BY
-  success_count DESC;
-
-
--- Top 10 projects with the most difference between money pledged and money goal
-SELECT
-  name,
-  (pledged - goal) AS difference
-FROM
-  luisalva.crowdfunding_dataset.projects
-WHERE
-  status = 'Successful'
-ORDER BY
-  difference DESC
-LIMIT 10;
 
 
 -- Successful projects above average money pledged
@@ -133,6 +76,77 @@ WHERE
     luisalva.crowdfunding_dataset.projects
   WHERE
     status = 'Successful');
+
+
+-- Average backers by category
+SELECT
+  category,
+  ROUND(AVG(backers), 2) AS avg_backers
+FROM
+  luisalva.crowdfunding_dataset.projects
+GROUP BY
+  category
+ORDER BY
+  avg_backers DESC;
+
+
+---------------COUNTRY
+  
+-- Projects by country
+SELECT
+  country,
+  COUNT(project_id) AS project_count
+FROM
+  luisalva.crowdfunding_dataset.projects
+GROUP BY
+  country
+ORDER BY
+  project_count DESC;
+
+
+-- Countries with most succesful projects
+SELECT
+  country,
+  COUNT(status) AS success_count
+FROM
+  luisalva.crowdfunding_dataset.projects
+WHERE
+  status = 'Successful'
+GROUP BY
+  country
+ORDER BY
+  success_count DESC;
+
+
+-- Country with most backers 
+SELECT
+  country,
+  SUM(backers) AS avg_backers
+FROM
+  luisalva.crowdfunding_dataset.projects
+GROUP BY
+  country
+ORDER BY
+  avg_backers DESC;
+
+
+-- Countries with most money pledged
+-- AVerage goal per country 
+
+
+
+-- Top 10 projects with the most difference between money pledged and money goal--- add category
+SELECT
+  category,
+  name,
+  (pledged - goal) AS difference
+FROM
+  luisalva.crowdfunding_dataset.projects
+WHERE
+  status = 'Successful'
+ORDER BY
+  difference DESC
+LIMIT 10;
 
 
 -- Average difference between money pledged and money goal for succesul project status by category
